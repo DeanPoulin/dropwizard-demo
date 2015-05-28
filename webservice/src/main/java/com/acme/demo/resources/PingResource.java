fa0bcfc04c7960c3d0079199f11367d514797684
Dean W Poulin <dpoulin@healthmarketscience.com>
2015-05-28 18:08:30 -0400

    Removed swagger integration. Not yet open source.

--- webservice/src/main/java/com/acme/demo/resources/PingResource.java
+++ webservice/src/main/java/com/acme/demo/resources/PingResource.java
@@ -8,21 +8,17 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.stereotype.Component;
 
 import com.acme.demo.webservice.model.Pong;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
 import com.yammer.dropwizard.jersey.caching.CacheControl;
 import com.yammer.metrics.annotation.Timed;
 
 @Path("/ping")
 @Component
-@Api(value = "/ping", description = "Ping Resource")
 public class PingResource {
     
     @Timed
     @GET
     @Path("/")
     @CacheControl(isPrivate = true, maxAge = 0, noCache = true)
-    @ApiOperation(value = "Ping", notes = "Respond to Ping Requests", response = Pong.class)
     @Produces({ MediaType.APPLICATION_JSON })
     public Pong ping() {
         Pong response = new Pong();
